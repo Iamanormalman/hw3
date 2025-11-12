2493ec7505e98cf9b1b7addad0e8ff0ad60eca64
Eric Lin <lin@gmail.com>
2025-11-12 14:27:26 +0800

    Initial commit

---
+++ ScoreManager.java
@@ -0,0 +1,30 @@
+public class ScoreManager {
+
+    public void addStudent(String name, int[] scores){
+
+    }
+
+    public void deleteStudent(String name){
+
+    }
+
+    public showIndividualScore(String name){
+
+    }
+
+    public showIndividualAverage(String name){
+
+    }
+
+    public showHomeworkAverage(int number){
+
+    }
+
+    public showFinalProjectAverage(int number){
+
+    }
+
+    public showTotal(){
+
+    }
+}
