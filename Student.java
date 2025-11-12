2ed3452137b0be7a177eb4d00040a0f8f9fe404a
Eric Lin <lin@gmail.com>
2025-11-12 14:01:16 +0800

    Initial commit

---
+++ Student.java
@@ -0,0 +1,20 @@
+public class Student {
+    private String name;
+    private Int[] score;
+
+    public String getName() {
+        return name;
+    }
+
+    public Int[] getScore() {
+        return score;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setScore(Int[] score) {
+        this.score = score;
+    }
+}
