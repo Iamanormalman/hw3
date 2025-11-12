e4d9159ee60bda4596a9cdf944be3eab005ed5f3
Eric Lin <lin@gmail.com>
2025-11-12 16:42:11 +0800

    Initial commit

--- hw3.java
+++ hw3.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 public class hw3 {
     public static void main(String[] args) throws Exception{
         if(args.length != 1){
-            System.out.println("只能有一個檔案");
+            System.out.println("必須且只能有一個檔案");
             System.exit(0);
         }
 
         ScoreManager scoreManager = new ScoreManager();
 
         File file = new File(args[0]);
-        Scanner scanner = new Scanner(file);
-        while(scanner.hasNextLine()) {
-            String[] str = scanner.nextLine().split("\\s+");
+        Scanner reader = new Scanner(file);
+        while(reader.hasNextLine()) {
+            String[] str = reader.nextLine().split("\\s+");
             String name = str[0];
             int[] scores = new int[5];
             for(int i=1;i<6;i++){
@@ -21,5 +21,11 @@ public class hw3 {
             }
             scoreManager.addStudent(name, scores);
         }
+        reader.close();
+
+        System.out.println("歡迎使用成績管理系統")
+
+        Scanner input = new Scanner(System.in);
+        String command = input.nextLine();
     }
 }
